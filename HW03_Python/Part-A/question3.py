4724a7cbac01425bd8c6d997528ab69e94d037dc
Vishal1711 <jadhavvishal669@gmail.com>
2021-09-29 14:43:05 -0400

    HW03

--- HW03_Python/Part-A/question3.py
+++ HW03_Python/Part-A/question3.py
@@ -1,31 +1,101 @@
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
+    def __init__(self, val=0, next=0):
+        self.data = val
         self.next = next
 
 
 
-class Solution:
-    def addTwoNumbers(self, l1, l2):
+class LinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = ListNode(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Add contents of two linked lists and return the head
+    # node of resultant list
+    def addTwoLists(self, first, second):
+        prev = None
+        temp = None
         carry = 0
-        root = n = ListNode(0)
-        while l1 or l2 or carry:
-            v1 = v2 = 0
-            if l1:
-                v1 = l1.val
-                l1 = l1.next
-            if l2:
-                v2 = l2.val
-                l2 = l2.next
 
-            carry, val = divmod(v1+v2+carry, 10)
-            n.next = ListNode(val)
-            n = n.next
-        return root.next
+        # While both list exists
+        while(first is not None or second is not None):
 
+            # Calculate the value of next digit in
+            # resultant list
+            # The next digit is sum of following things
+            # (i) Carry
+            # (ii) Next digit of first list (if ther is a
+            # next digit)
+            # (iii) Next digit of second list ( if there
+            # is a next digit)
+            fdata = 0 if first is None else first.data
+            sdata = 0 if second is None else second.data
+            Sum = carry + fdata + sdata
 
-l1 = [2, 0, 3] # Array of numbers
-l2 = [4, 3, 3]
-s = Solution()
-print(s.addTwoNumbers(l1, l2))
+            # update carry for next calculation
+            carry = 1 if Sum >= 10 else 0
+
+            # update sum if it is greater than 10
+            Sum = Sum if Sum < 10 else Sum % 10
+
+            # Create a new node with sum as data
+            temp = ListNode(Sum)
+
+            # if this is the first node then set it as head
+            # of resultant list
+            if self.head is None:
+                self.head = temp
+            else:
+                prev.next = temp
+
+            # Set prev for next insertion
+            prev = temp
+
+            # Move first and second pointers to next nodes
+            if first is not None:
+                first = first.next
+            if second is not None:
+                second = second.next
+
+        if carry > 0:
+            temp.next = ListNode(carry)
+
+    # Utility function to print the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+# Driver code
+first = LinkedList()
+second = LinkedList()
+
+# Create first list
+first.push(2)
+first.push(4)
+first.push(3)
+
+print ("First List is ")
+first.printList()
+
+# Create second list
+second.push(5)
+second.push(6)
+second.push(4)
+print ("\nSecond List is ")
+second.printList()
+
+# Add the two lists and see result
+res = LinkedList()
+res.addTwoLists(first.head, second.head)
+print ("\nResultant list is ")
+res.printList()
